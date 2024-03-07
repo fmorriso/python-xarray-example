f0a2b23c729bae23d77882fc5f51244c1b7d9a7a
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-07 11:39:58 -0700

    initial commit

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
-# Press the green button in the gutter to run the script.
 def simple_x_y():
-    x_sample = [1,2,3,4]
+    x_sample = [1, 2, 3, 4]
     x_arry = [x_sample] * 3
     # print(f'{x_arry=}')
 
     y1 = [1] * 4
     y2 = [2] * 4
     y3 = [3] * 4
-    y_arry = [ y1, y2, y3]
+    y_arry = [y1, y2, y3]
     # print(f'{y_arry=}')
 
     x = np.array(x_arry)
@@ -25,8 +24,6 @@ def simple_x_y():
     print(f'{y=}')
 
 
-
-
 if __name__ == '__main__':
-    print(f'Xarray Example using python version {get_python_version()}')
+    print(f'Xarray Example using python version {get_python_version()}, XArray version {xr.__version__} and NumPy {np.__version__}')
     simple_x_y()
---
+++ readme.md
@@ -0,0 +1,12 @@
+# XArray Example
+## References
+* [Building with XArray from scratch - video](https://www.youtube.com/watch?v=hiwDEjv2a60&list=PLNemzZpJM7lUu_iGP_lA2m7SeSUwKSIvR&index=6)
+* [XArray Get Started](https://tutorial.xarray.dev/overview/get-started.html)
+## History
+* 2024-03-07 - initial creation
+## Tools Used (initially)
+* Python 3.12.2
+* XArray 2024.2.0
+* NumPy 1.26.4
+* PyCharm 2023.3.4
+* VSCode 1.87.0
